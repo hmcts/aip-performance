ccad3832cf99361c36de8956e3b9ac7a0f4042ec
DeepakGarcha <61466880+DeepakGarcha@users.noreply.github.com>
2021-07-06 15:44:10 +0100

    First Commit

---
+++ src/gatling/simulations/uk/gov/hmcts/reform/cmc/performance/scenarios/AIP_Appeal.scala
@@ -0,0 +1,427 @@
+
+package uk.gov.hmcts.reform.cmc.performance.scenarios
+import java.io.{BufferedWriter, FileWriter}
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import uk.gov.hmcts.reform.cmc.performance.scenarios.utils.{CsrfCheck, CurrentPageCheck, Environment}
+import uk.gov.hmcts.reform.cmc.performance.scenarios.utils.CsrfCheck.{csrfParameter, csrfTemplate}
+import uk.gov.hmcts.reform.cmc.performance.scenarios.utils.CurrentPageCheck.currentPageTemplate
+
+import scala.concurrent.duration._
+
+/*AIP Appeal is a new Journey which allows an appellant to go through several eligibility questions and if successful
+they can login to the new AIP portal and start an appeal. This script carries out this business journey all the way
+until the appeal is submitted and then captures and stores the appeal reference which we can use in business journeys
+which are still to be developed*/
+
+object AIP_Appeal {
+
+val BaseURL = Environment.baseURL
+val IdAMURL = Environment.idamURL
+val MinThinkTime = Environment.minThinkTime
+val MaxThinkTime = Environment.maxThinkTime
+val CommonHeader = Environment.commonHeader
+val paymentURL = Environment.PaymentURL
+val TotalAmount = scala.util.Random.nextInt(9999)
+val aipuser = csv("AIPUser.csv").circular
+val aipuserdetails = csv("AIPUserDetails.csv").circular
+val holetterdetails = csv("HOLetterDetails.csv").circular
+val personaldetails = csv("PersonalDetails.csv").circular
+
+  //AIP Start Appeal HomePage
+  val home = group ("AIP_010_Homepage") {
+    exec(flushHttpCache).exec(flushSessionCookies).exec(flushCookieJar)
+      .exec(http("AIP_010_Homepage")
+        .get("/start-appeal")
+        .check(CurrentPageCheck.save)
+        //.check(CsrfCheck.save)
+        .check(status.is(200))
+        .check(regex("Appeal an immigration or asylum decision"))
+        .headers(Environment.headers_2))
+      .exitHereIfFailed
+    }
+    .pause(MinThinkTime, MaxThinkTime)
+
+      //User first answers eligibility questions before he is allowed to login.  First question - Are you currently in UK
+      val eligibility =group("AIP_020_eligibility_GET") {
+        exec(http("AIP_020_eligibility_GET")
+          .get("/eligibility")
+          .check(CurrentPageCheck.save)
+          .check(CsrfCheck.save)
+          .check(status.is(200))
+          .check(regex("Are you currently living in the United Kingdom"))
+          .headers(Environment.headers_19))
+      }
+        .pause(MinThinkTime, MaxThinkTime)
+
+        // Eligibity - Q1 Are you living in UK - Answers yes
+        .group("AIP_030_eligibility_Question1_POST") {
+          exec(http("AIP_030_eligibility_Question1_POST")
+            .post("/eligibility")
+            .headers(Environment.headers_34)
+            .formParam("_csrf", "${csrf}")
+            .formParam("questionId", "0")
+            .formParam("answer", "yes")
+            .formParam("continue", "")
+            .check(status.is(200))
+            .check(CsrfCheck.save))
+        }
+        .pause(MinThinkTime, MaxThinkTime)
+
+        // Eligibity - Q2 Are you in Detention - Answers no
+        .group("AIP_040_eligibility_Question2_POST") {
+          exec(http("AIP_040_eligibility_Question2_POST")
+            .post("/eligibility")
+            .headers(Environment.headers_34)
+            .formParam("_csrf", "${csrf}")
+            .formParam("questionId", "1")
+            .formParam("answer", "no")
+            .formParam("continue", "")
+            .check(status.is(200))
+            .check(CsrfCheck.save))
+        }
+
+        .pause(MinThinkTime, MaxThinkTime)
+
+        // Eligibity - Q3 Are you Appealing an asylum or Humanitarian Decision - Answers Yes
+        .group("AIP_050_eligibility_Question3_POST") {
+          exec(http("AIP_050_eligibility_Question3_POST")
+            .post("/eligibility")
+            .headers(Environment.headers_34)
+            .formParam("_csrf", "${csrf}")
+            .formParam("questionId", "2")
+            .formParam("answer", "yes")
+            .formParam("continue", "")
+            .check(status.is(200)))
+        }
+
+        .pause(MinThinkTime, MaxThinkTime)
+
+  // Users gets to Login Page after successfully answering the eligibility questions
+  val Login =group("AIP_060_Login_GET") {
+    exec (http ("AIP_060_Login_GET")
+      .get("/login?register=true")
+      .headers(Environment.headers_19)
+      .check (status.is (200))
+      .check(CsrfCheck.save))
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Login into Application with an IAC Citizen account
+    .group("AIP_080_Login_POST") {
+      feed(aipuser)
+       .exec(http("AIP_080_Login_POST")
+         .post(IdAMURL + "/login?redirect_uri=https%3a%2f%2fimmigration-appeal.perftest.platform.hmcts.net%2fredirectUrl&client_id=iac&state=b14effe6-2e3d-4ee0-8dab-24dfe97ee42f&scope=")
+         .headers(Environment.headers_113)
+         .formParam("username", "${aipuser}")
+         .formParam("password", "${password}")
+         .formParam("save", "Sign in")
+         .formParam("selfRegistrationEnabled", "true")
+         .formParam("_csrf", "${csrf}")
+         .check (status.is (200))
+         .check(regex("Your appeal details")))
+    }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //User gets the About Appeal Page after the have logged in
+  val AboutAppeal =group("AIP_090_AboutAppeal_GET") {
+      exec(http("AIP_090_AboutAppeal_GET")
+      .get("/about-appeal")
+      .headers(Environment.headers_19)
+      .check(regex("Tell us about your appeal"))
+      .check (status.is (200)))
+
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //User clicks on Home Office & gets Home Office Reference Number Page
+  val HomeOffice =group("AIP_100_HomeOffice_GET") {
+    exec(http("AIP_100_HomeOffice_GET")
+    .get("/home-office-reference-number")
+    .headers(Environment.headers_19)
+    .check (status.is (200))
+    .check(CsrfCheck.save)
+    .check(regex("What is your Home Office reference number")))
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Enter Home Office Letter Reference - this can be any 10 digit number
+  .group("AIP_110_HomeOffice_POST") {
+    feed(holetterdetails)
+    .exec(http("AIP_110_HomeOffice_POST")
+    .post("/home-office-reference-number")
+    .headers(Environment.headers_34)
+    .formParam("_csrf", "${csrf}")
+    .formParam("homeOfficeRefNumber", "${HORef}")
+    .formParam("saveAndContinue", "")
+   //.resources(http("request_162")
+    .check(CsrfCheck.save)
+    .check(regex("What date was your decision letter sent"))
+    .check (status.is (200)))
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Date Home Office Letter Sent this date has to be within the last 30 days
+  .group("AIP_120_HomeOffice_DateLetterSent") {
+    feed(holetterdetails)
+    .exec(http("AIP_120_HomeOffice_DateLetterSent")
+    .post("/date-letter-sent")
+      .headers(Environment.headers_34)
+      .formParam("_csrf", "${csrf}")
+      .formParam("day", "${day}")
+      .formParam("month", "${month}")
+      .formParam("year", "${year}")
+      .formParam("saveAndContinue", "")
+      .check(CsrfCheck.save)
+      .check(regex("Upload your Home Office decision letter"))
+      .check (status.is (200)))
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+    //User uploads the decision letter
+    .group("AIP_130A_HomeOffice_UploadDecisionLetter") {
+      exec(http("AIP_130A_HomeOffice_UploadDecisionLetter")
+      .post("/home-office-upload-decision-letter/upload?_csrf=${csrf}")
+      .headers(Environment.headers_0)
+      .header("content-Type", "multipart/form-data; boundary=----WebKitFormBoundaryBOgHbWr4LuU2ZuBi")
+        .bodyPart(RawFileBodyPart("file-upload", "HORefusal.pdf")
+          .fileName("HORefusal.pdf")
+          .transferEncoding("binary"))
+        .asMultipartForm
+        .formParam("classification", "PUBLIC")
+    .check(CsrfCheck.save)
+    .check(status in(200, 304)))
+    }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+    //Checks that the file has been uploaded successfully
+    .group("AIP_130C_HomeOffice_UploadDecisionLetter") {
+      exec(http("AIP_130C_HomeOffice_UploadDecisionLetter")
+        .get("/home-office-upload-decision-letter")
+        .headers(Environment.headers_19)
+        .check (status.is (200))
+        .check(regex("HORefusal.pdf"))
+        .check(CsrfCheck.save))
+    }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+    //Save the Decision letter file
+    .group("AIP_130B_HomeOffice_UploadDecisionLetter") {
+      exec(http("AIP_130B_HomeOffice_UploadDecisionLetter")
+      .post("/home-office-upload-decision-letter")
+        .headers(Environment.headers_17)
+        .formParam("_csrf", "${csrf}")
+        .formParam("file-upload", "")
+        .formParam("saveAndContinue", "")
+        .check(regex("Tell us about your appeal"))
+        .check (status.is (200)))
+    }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Select Enter Personal Details
+  val PersonalDetails =
+
+    group("AIP_140_Name_GET") {
+    feed(personaldetails)
+    .exec(http("AIP_140_Name_GET")
+    .get("/name")
+    .headers(Environment.headers_19)
+    .check (status.is (200))
+    .check(regex("What is your name?"))
+    .check(CsrfCheck.save))
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Enter Name Details
+  .group("AIP_150_Name_Post") {
+    feed(personaldetails)
+    .exec(http("AIP_150_Name_Post")
+    .post("/name")
+      .headers(Environment.headers_34)
+      .formParam("_csrf", "${csrf}")
+      .formParam("givenNames", "${givenname}")
+      .formParam("familyName", "${familyname}")
+      .formParam("saveAndContinue", "")
+      .check(CsrfCheck.save)
+      .check (status.is (200)))
+  }
+
+   .pause(MinThinkTime, MaxThinkTime)
+
+  //Enter Date of Birth Details
+  .group("AIP_160_Date-Birth_Post") {
+    feed(personaldetails)
+    .exec(http("AIP_160_Date-Birth_Post")
+    .post("/date-birth")
+    .headers(Environment.headers_34)
+    .formParam("_csrf", "${csrf}")
+    .formParam("day", "${day}")
+    .formParam("month", "${month}")
+    .formParam("year", "${year}")
+    .formParam("saveAndContinue", "")
+    //.resources(http("request_264")
+    .check(CsrfCheck.save)
+    .check (status.is (200)))
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Enter Nationality
+  .group("AIP_170_Nationality_Post") {
+    feed(personaldetails)
+    .exec(http("AIP_170_Nationality_Post")
+    .post("/nationality")
+      .headers(Environment.headers_34)
+      .formParam("_csrf", "${csrf}")
+      .formParam("nationality", "${nationality}")
+      .formParam("saveAndContinue", "")
+      .check(CsrfCheck.save)
+      .check (status.is (200)))
+  }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Go to page toEnter Address Manually as the post code lookup is failing
+  .group("AIP_180_Manual_Address_Get") {
+    exec(http("AIP_180_Manual_Address_Get")
+    .get("/manual-address")
+    .headers(Environment.headers_19)
+    .check(CsrfCheck.save)
+    .check (status.is (200)))
+  }
+    .pause(MinThinkTime, MaxThinkTime)
+
+  //Enter Address Manually
+  .group("AIP_190_Manual_Address_Post"){
+  exec(http("AIP_190_Manual_Address_Get")
+    .post("/manual-address")
+    .headers(Environment.headers_34)
+    .formParam("_csrf", "${csrf}")
+    .formParam("address-line-1", "Flat 21")
+    .formParam("address-line-2", "214 Westferry Road")
+    .formParam("address-town", "London")
+    .formParam("address-county", "London")
+    .formParam("address-postcode", "e14 3rr")
+    .formParam("saveAndContinue", "")
+    .check (status.is (200)))
+    //.check(CsrfCheck.save))
+    }
+
+    .pause(MinThinkTime, MaxThinkTime)
+
+//Go to contact preferences page
+  val ContactDetails =group("AIP_200_ContactPreferences_GET") {
+    exec(http("AIP_200_ContactPreferences_GET")
+    .get("/contact-preferences")
+    .headers(Environment.headers_19)
+    .check(CsrfCheck.save)
+    .check (status.is (200)))
+  }
+
+  .pause(MinThinkTime, MaxThinkTime)
+
+  //Enter contact preferences
+ .group("AIP_210_ContactPreferences_POST") {
+    exec(http("AIP_210_ContactPreferences_POST")
+    .post("/contact-preferences")
+    .headers(Environment.headers_34)
+    .formParam("_csrf", "${csrf}")
+    .formParam("selections", "email")
+    .formParam("email-value", "perftestiac001@gmail.com")
+    .formParam("text-message-value", "")
+    .formParam("saveAndContinue", "")
+    .check (status.is (200)))
+//    .check(CsrfCheck.save))
+  }
+
+  .pause(MinThinkTime, MaxThinkTime)
+
+  //Go to Type of Appeal Page
+  val TypeofAppeal =group("AIP_220_AppealType_GET") {
+    exec(http("AIP_220_AppealType_GET")
+    .get("/appeal-type")
+    .headers(Environment.headers_19)
+    .check (status.is (200))
+    .check(CsrfCheck.save))
+  }
+
+  .pause(MinThinkTime, MaxThinkTime)
+
+  //Enter Type of appeal - Protection
+  .group("AIP_230_AppealType_POST") {
+    exec(http("AIP_230_AppealType_POST")
+    .post("/appeal-type")
+      .headers(Environment.headers_34)
+      .formParam("_csrf", "${csrf}")
+      .formParam("appealType", "protection")
+      .formParam("saveAndContinue", "")
+      .check (status.is (200)))
+  }
+
+  .pause(MinThinkTime, MaxThinkTime)
+
+  //Verifies the information entered is valid
+  val CheckAnswers =group("AIP_240_CheckAnswers_GET") {
+    exec(http("AIP_240_CheckAnswers_GET")
+    .get("/check-answers")
+    .headers(Environment.headers_19)
+    .check (status.is (200))
+    .check(regex("Check your answer"))
+    .check(regex("I believe the information I have given is true"))
+    .check(CsrfCheck.save))
+  }
+
+  .pause(MinThinkTime, MaxThinkTime)
+
+    //Accepts the details are valid and submits the appeal
+   .group("AIP_250_CheckAnswers_POST") {
+     exec(http("AIP_250_CheckAnswers_POST")
+    .post("/check-answers")
+    .headers(Environment.headers_34)
+    .formParam("_csrf", "${csrf}")
+    .formParam("statement", "acceptance")
+    .check(regex("Your appeal details have been sent"))
+    .check (status.is (200)))
+   }
+
+  .pause(MinThinkTime, MaxThinkTime)
+
+  //User goes tot the Appeal Overview page and gets the reference  number for the appeal.  This is stored in
+  // AIPAppeakRef.csv by the script.  this can be used for future business journeys once they are developed
+  val AppealOverview =group("AIP_260_AppealOverview_GET") {
+    exec(http("AIP_260_AppealOverview_GET")
+    .get("/appeal-overview")
+    .headers(Environment.headers_19)
+      .check(regex("""Appeal reference:(.*)</p>""").saveAs("AppealRef"))
+      .check (status.is (200)))
+
+    .exec { session =>
+      val fw = new BufferedWriter(new FileWriter("AIPAppealRef.csv", true))
+      try {
+        fw.write(session("AppealRef").as[String] + "\r\n")
+      } finally fw.close()
+      session
+    }
+  }
+
+  .pause(MinThinkTime, MaxThinkTime)
+
+  val AIPLogout =group("AIP_270_Logout_GET") {
+    exec(http("AIP_270_Logout_GET")
+    .get("/logout")
+    .headers(Environment.headers_19)
+    .check (status.is (200)))
+  //  .check(CsrfCheck.save))
+  }
+}
---
+++ src/gatling/simulations/uk/gov/hmcts/reform/cmc/performance/scenarios/utils/Common.scala
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.cmc.performance.scenarios.utils
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import scala.util.Random
+
+object Common {
+
+  val rnd = new Random()
+  val now = LocalDate.now()
+  val patternDay = DateTimeFormatter.ofPattern("dd")
+  val patternMonth = DateTimeFormatter.ofPattern("MM")
+  val patternYear = DateTimeFormatter.ofPattern("yyyy")
+
+  def randomString(length: Int) = {
+    rnd.alphanumeric.filter(_.isLetter).take(length).mkString
+  }
+  
+  def randomRepeatInteger() : Integer=
+  {
+    val list=List(300, 280, 260, 250, 240, 200,220,350)
+    //val list=List(2,3)
+    val start = 200
+    val end   = 300
+    val rnd = new scala.util.Random
+    list(rnd.nextInt(list.length))
+  }
+  
+  import scala.util.Random
+  object MyClass {
+    def main(args: Array[String]) {
+      val list = List(12, 65, 89, 41, 99, 102)
+      val random = new Random
+      println("Random value of the list " + list(random.nextInt(list.length)))
+    }
+  }
+
+
+ def getDay(): String = {
+    (1 + rnd.nextInt(28)).toString
+  }
+
+  def getMonth(): String = {
+    (1 + rnd.nextInt(12)).toString
+  }
+  /*
+  def getDay(): String = {
+    (10 + rnd.nextInt(28)).toString.format(patternDay).reverse.padTo(2, '0').reverse //pads single-digit dates with a leading zero
+  }
+
+  def getMonth(): String = {
+    (11 + rnd.nextInt(12)).toString.format(patternMonth).reverse.padTo(2, '0').reverse //pads single-digit dates with a leading zero
+  }
+  */
+  //Dod <= 21 years
+  def getYear(): String = {
+    now.minusYears(30 + rnd.nextInt(50)).format(patternYear)
+  }
+
+/*
+  val reasonForAppealing = "Social Security and Child Support forms including notices of appeal to the Department of Work and Pensions and HM Revenue and Customs.
+    You can appeal a decision about your entitlement to benefits, for example Personal Independence Payment (PIP), Employment and Support Allowance (ESA) and Universal Credit.
+    Appeals are decided by the Social Security and Child Support Tribunal. The tribunal is impartial and independent of government. The tribunal will listen to both sides before making a decision.
+    You can appeal a decision about your entitlement to benefits, for example Personal Independence Payment (PIP), Employment and Support Allowance (ESA) or Universal Credit.
+    If you do not need a mandatory reconsideration your decision letter will say why. Include this explanation when you submit your appeal.
+    You’ll need to choose whether you want to go to the tribunal hearing to explain your appeal in person. If you do not attend, your appeal will be decided on your appeal form and any supporting evidence you provide."
+
+  val anythingElse = "Things that were not considered are. 
+    You can appoint someone as a ‘representative’ to help you with your appeal. A representative can.help you submit your appeal or prepare your evidence act on your behalf give you advice. Anyone can be a representative, including friends and family.
+    You might also be able to find a representative through a library or from an organisation in your area that gives advice on claiming benefits, such as Citizens Advice.
+    Your representative will have permission to act on your behalf, for example to respond to letters. They’ll be sent all the information about your appeal, including any medical evidence."
+*/
+}
