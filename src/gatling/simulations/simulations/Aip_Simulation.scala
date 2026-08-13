34e421568201a9aad4fe2864fe0475680c19ffbe
pkryonas <paris.kryonas@solirius.com>
2026-08-13 11:46:48 +0100

    Removing failing step due to journey change.

--- src/gatling/simulations/simulations/Aip_Simulation.scala
+++ src/gatling/simulations/simulations/Aip_Simulation.scala
@@ -101,8 +101,8 @@ class Aip_Simulation extends Simulation {
         )))
       .pause(1)
       .exec(CcdHelper.addCaseEvent("#{cw-username}", "#{cw-password}", CcdCaseTypes.IA_Asylum, "#{caseId}", "requestHomeOfficeData", "bodies/CCD_requestHomeOfficeData.json"))
-      .pause(1)
-      .exec(CcdHelper.addCaseEvent("#{cw-username}", "#{cw-password}", CcdCaseTypes.IA_Asylum, "#{caseId}", "requestRespondentEvidence", "bodies/CCD_requestRespondentEvidence.json"))
+      //.pause(1)
+      //.exec(CcdHelper.addCaseEvent("#{cw-username}", "#{cw-password}", CcdCaseTypes.IA_Asylum, "#{caseId}", "requestRespondentEvidence", "bodies/CCD_requestRespondentEvidence.json"))
       .exec{
         session =>
           println(session)
